03258b50e190a82083db41828c19b95694cbd766
Danial <dtoor@wgu.edu>
2023-11-26 22:31:56 -0600

    updated lists

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
-
 import { useState, useEffect } from "react";
 
-function Form() {
-  return <></>;
-}
-
-function App() {
+function Form({ onSubmit }) {
   const [name, setName] = useState("");
   const [count, setCount] = useState(0);
-  const [listOfItems, setListOfItems] = useState([]);
 
-  const updateItems = (newItem, newAmount) => {
-    setName(newItem);
-    setCount(newAmount);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(name, count);
+    // Clear the input fields after submitting
     setName("");
     setCount(0);
   };
 
-  useEffect(() => {
-    setListOfItems([{ itemName: name, amount: count }]);
-  }, [name, count]);
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Amount"
+        value={count}
+        onChange={(e) => setCount(parseInt(e.target.value, 10))}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const updateItems = (newItem, newAmount) => {
+    setItems([...items, { itemName: newItem, amount: newAmount }]);
+  };
 
   return (
     <div className="container">
@@ -29,47 +46,19 @@ function App() {
       <p className="form-header">
         Form: What do you need for your trip. # + name + add
       </p>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          updateItems(name, count);
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></input>
-        <input
-          type="number"
-          placeholder="Amount"
-          value={count}
-          onChange={(e) => setCount(parseInt(e.target.value, 10))}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>
-        Item, Item, Item... CheckMark it(Cross it off) , X it off. sort by,clear
-        all
-      </p>
-      <p>You have xxx items,, you packed xx%</p>
+      <Form onSubmit={updateItems} />
       <ul>
-        {listOfItems.map((item, index) => (
+        {items.map((item, index) => (
           <li key={index}>{`${item.itemName}: ${item.amount}`}</li>
         ))}
       </ul>
+      <p>
+        Item, Item, Item... CheckMark it(Cross it off), X it off. sort by, clear
+        all
+      </p>
+      <p>You have {items.length} items, you packed xx%</p>
     </div>
   );
 }
 
 export default App;
-
-/*
-
-<Logo />
-<Form />
-<PackingList />
--> <Item />
-<Stats>
-*/
